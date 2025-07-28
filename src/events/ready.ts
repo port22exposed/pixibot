4f3141d63920646d633caeadb515e7a841f143d7
port22exposed <scm@port22.exposed>
2025-07-28 05:50:09 +0100

    better online message

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -3,6 +3,6 @@ import discord from 'discord.js-selfbot-v13'
 export const once = true
 
 export async function execute(client: discord.Client) {
-	console.log(client.user?.displayName, 'is online')
+	console.log(client.user?.username, 'is online')
 	client.user?.setStatus('invisible')
 }

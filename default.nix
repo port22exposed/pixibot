d9ebac3367acafbd86cb7a42fa18d7b402d63813
port22exposed <scm@port22.exposed>
2025-07-28 04:26:08 +0100

    fix nodejs version

--- default.nix
+++ default.nix
@@ -1,9 +1,11 @@
-{ lib, buildNpmPackage }:
+{ lib, buildNpmPackage, nodejs_latest }:
 buildNpmPackage {
   pname = "pixibot";
   version = "0.1.0";
 
   src = ./.;
+
+  nodejs = nodejs_latest;
 
   npmDepsHash = lib.fakeHash;
 }

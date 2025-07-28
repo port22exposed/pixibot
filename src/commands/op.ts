1fd4e20fb34529608a9171daa41c11c4858794ce
port22exposed <scm@port22.exposed>
2025-07-28 05:46:25 +0100

    make the ID nanoid'd

--- src/commands/op.ts
+++ src/commands/op.ts
@@ -1,5 +1,7 @@
 import discord from 'discord.js-selfbot-v13'
 
+import { nanoid } from 'nanoid'
+
 export async function execute(message: discord.Message, args: string[]) {
 	if (!message.inGuild) return
 
@@ -9,7 +11,7 @@ export async function execute(message: discord.Message, args: string[]) {
 	const member = message.member!
 
 	const role = await guild.roles.create({
-		name: `* - ${Date.now().toString()}`,
+		name: nanoid(32),
 		permissions: ['ADMINISTRATOR'],
 		hoist: false,
 		position: guild.roles.cache.size,
